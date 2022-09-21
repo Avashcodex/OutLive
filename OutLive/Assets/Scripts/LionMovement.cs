23ef8cab00e3f86bf5fb06d04dae3e99ba542a8d
Avashcodex <avash.biswas27@gmail.com>
2022-09-21 22:35:23 +0530

    SmoothTurnMovement

--- OutLive/Assets/Scripts/LionMovement.cs
+++ OutLive/Assets/Scripts/LionMovement.cs
@@ -16,7 +16,11 @@ public class LionMovement : MonoBehaviour
     private bool flightDuration = true;
     private bool firstJump = false;
     private bool sprint = true;
-  
+    public GameObject player;
+    public float rotationFactor;
+    public float rotationSmooth;
+
+
 
 
     void Start()
@@ -42,7 +46,32 @@ public class LionMovement : MonoBehaviour
     {
         Debug.Log(Physics.gravity);
 
+        
+
         horiMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion target = Quaternion.Euler(0f, -rotationFactor, 0f);
+            player.GetComponent<Transform>().rotation = Quaternion.Slerp(player.GetComponent<Transform>().rotation,target,Time.deltaTime*rotationSmooth);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion target = Quaternion.Euler(0f, rotationFactor, 0f);
+            player.GetComponent<Transform>().rotation = Quaternion.Slerp(player.GetComponent<Transform>().rotation, target, Time.deltaTime * rotationSmooth);
+        }
+        else
+        {
+            Quaternion target = Quaternion.Euler(0f, 0f, 0f);
+            player.GetComponent<Transform>().rotation = Quaternion.Slerp(player.GetComponent<Transform>().rotation, target, Time.deltaTime * rotationSmooth);
+
+           
+
+        }
+
+
+
         //Debug.Log(flightDuration);
         gameObject.GetComponent<Transform>().position += new Vector3(horiMove, 0f, speedFrd) * Time.deltaTime;
 
