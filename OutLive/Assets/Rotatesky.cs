ab962abda0f7e85e95013f78ad36342356693f14
Destrocoder <yomahbusareb22@gmail.com>
2022-09-22 23:06:39 +0530

    Terrain

---
+++ OutLive/Assets/Rotatesky.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotatesky : MonoBehaviour
+{
+    public float Rotatespeed = 1.2f;
+ 
+    void Update()
+    {
+       RenderSettings.skybox.SetFloat("_Rotation", Time.time * Rotatespeed);
+        
+    }
+}
